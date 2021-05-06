3d16ac4b50f9369ba0930665e68e991d6b80d98f
Kitae Kim <develoot@gmail.com>
2021-05-06 23:13:26 +0900

    Fix package name and missing dependency

--- src/main/java/edu/knu/se/movierecommendation/UserRepository.java
+++ src/main/java/edu/knu/se/movierecommendation/UserRepository.java
@@ -1,9 +1,9 @@
-package edu.knu.se;
+package edu.knu.se.movierecommendation;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 interface UserRepository extends JpaRepository<User, Long> {
     User findByUid(String uid);
-
     List<User> removeByUid(String uid);
 }
