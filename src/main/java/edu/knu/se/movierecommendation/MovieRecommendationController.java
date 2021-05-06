cd8e52d9cffb4b22123173c6c262d56cfd5d22f2
Kitae Kim <develoot@gmail.com>
2021-05-06 22:50:08 +0900

    Implement method `addUser()`

--- src/main/java/edu/knu/se/movierecommendation/MovieRecommendationController.java
+++ src/main/java/edu/knu/se/movierecommendation/MovieRecommendationController.java
@@ -1,14 +1,21 @@
 package edu.knu.se.movierecommendation;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MovieRecommendationController {
 
     @GetMapping("/")
-    public String indexPage() {
+    public String greeting() {
         return "Hello, world!";
+    }
+
+    @PutMapping("/users")
+    public String addUser(User user) {
+        boolean result = (userRepository.save(user) == null);
+        return "{ \"result\": " + (result ? "SUCCESS" : "FAILED") + " }";
     }
 
 }
