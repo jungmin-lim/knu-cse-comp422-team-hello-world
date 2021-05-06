99572b5f913376d972c7a32b5b22965b73be0ac9
jungmin lim <jungmin.lim.cs@gmail.com>
2021-05-06 23:11:22 +0900

    Implement "UserRepository.java" (#5)

---
+++ src/main/java/edu/knu/se/movierecommendation/UserRepository.java
@@ -0,0 +1,9 @@
+package edu.knu.se;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface UserRepository extends JpaRepository<User, Long> {
+    User findByUid(String uid);
+
+    List<User> removeByUid(String uid);
+}
