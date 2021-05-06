5d5545cbd2d2b0e4d70b542b9699671fc231a7b9
Kitae Kim <develoot@gmail.com>
2021-05-06 20:00:30 +0900

    Add initial maven files

---
+++ src/main/java/edu/knu/se/movierecommendation/MovieRecommendationApplication.java
@@ -0,0 +1,13 @@
+package edu.knu.se.movierecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovierecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovierecommendationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/knu/se/movierecommendation/MovieRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.knu.se.movierecommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovierecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
