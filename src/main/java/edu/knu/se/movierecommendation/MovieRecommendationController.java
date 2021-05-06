5f030a2aab410a84b5e4a3fc508992e4616bcea2
Kitae Kim <develoot@gmail.com>
2021-05-06 23:02:11 +0900

    Implement method `deleteUser()`

--- src/main/java/edu/knu/se/movierecommendation/MovieRecommendationController.java
+++ src/main/java/edu/knu/se/movierecommendation/MovieRecommendationController.java
@@ -1,5 +1,6 @@
 package edu.knu.se.movierecommendation;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,12 @@ public class MovieRecommendationController {
     @PutMapping("/users")
     public String addUser(User user) {
         boolean result = (userRepository.save(user) == null);
+        return "{ \"result\": " + (result ? "SUCCESS" : "FAILED") + " }";
+    }
+
+    @DeleteMapping("/users")
+    public String removeUser(String uid) {
+        boolean result = (userRepository.removeByUid(uid) == null);
         return "{ \"result\": " + (result ? "SUCCESS" : "FAILED") + " }";
     }
 
