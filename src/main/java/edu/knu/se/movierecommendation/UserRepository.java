e1066e640c17c91c6ccc9a23903ec9a3b69ab495
jungmin lim <jungmin.lim.cs@gmail.com>
2021-05-06 23:04:26 +0900

    Add removeByUid method for UserRepository.java

--- src/main/java/edu/knu/se/movierecommendation/UserRepository.java
+++ src/main/java/edu/knu/se/movierecommendation/UserRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 interface UserRepository extends JpaRepository<User, Long> {
     User findByUid(String uid);
+
+    List<User> removeByUid(String uid);
 }
