1e69e7eb208a73dafca2c0c2fa850e58630f30a7
jungmin <jungmin.lim.cs@knu.ac.kr>
2021-06-09 23:34:52 +0900

    trying fixing junit test

--- src/test/java/edu/knu/se/movierecommendation/MovieRecommendationControllerTest.java
+++ src/test/java/edu/knu/se/movierecommendation/MovieRecommendationControllerTest.java
@@ -19,7 +19,7 @@ import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class MovieRecommendationControllerTest {
